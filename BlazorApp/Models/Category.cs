10b19bf48995d8f27f446c7d67fde8a00809f37d
a.khapal <a.khapal@linker.by>
2021-03-04 15:56:51 +0300

    add competitors creation

--- BlazorApp/Models/Category.cs
+++ BlazorApp/Models/Category.cs
@@ -11,10 +11,11 @@ namespace BlazorApp.Data
         public string Name { get; set; }
         public string Type { get; set; }
         public string Age { get; set; }
-        public int AgeFrom{get;set;}
+        public int AgeFrom { get; set; }
         public int AgeTo { get; set; }
         public string Sex { get; set; }
         public int WeightFrom { get; set; }
         public int WeightTo { get; set; }
+        public int NumberOfCompetitors{get;set;}
     }
 }
