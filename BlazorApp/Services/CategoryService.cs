e1ea5f0f340d3af00e4eb76a7a173bd999f68de2
a.khapal <a.khapal@linker.by>
2021-03-11 15:38:02 +0300

    auto sending mails

--- BlazorApp/Services/CategoryService.cs
+++ BlazorApp/Services/CategoryService.cs
@@ -181,5 +181,15 @@ namespace BlazorApp.Data
         }
     };
         }
+
+        public List<string> GetEmails()
+        {
+            return new List<string>()
+            {
+                "1@mail.ru",
+                "workmail736@gmail.com",
+                "andreyrapterror@mail.ru"
+            };
+        }
     }
 }
