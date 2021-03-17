393c148d415b12d405dfd61e9b9a15abf2aa7193
a.khapal <a.khapal@linker.by>
2021-03-17 10:43:04 +0300

    Sportsman service Response object

--- BlazorApp/Services/SportsmenService.cs
+++ BlazorApp/Services/SportsmenService.cs
@@ -1,9 +1,9 @@
 ﻿using BlazorApp.Models;
+using BlazorApp.Util;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlazorApp.Services
 {
@@ -21,6 +21,61 @@ namespace BlazorApp.Services
                         .ToList();
                 return c;
             }
+        }
+        public ResponseObject CreateSportsman(Sportsman sportsman)
+        {
+            ResponseObject response = new ResponseObject();
+            try
+            {
+                using (var db = new ApplicationContext())
+                {
+                    db.Sportsmens.Add(sportsman);
+                    db.SaveChanges();
+                    response.IsSuccess = true;
+                }
+            }
+            catch(Exception e)
+            {
+                response.IsSuccess = false;
+                response.ExceptionMessage = e.Message;
+            }
+            return response;
+        }
+
+        public ResponseObject UpdateSportsman(Sportsman sportsman)
+        {
+            ResponseObject response = new ResponseObject();
+            try
+            {
+                using (var db = new ApplicationContext())
+                {
+                    db.Sportsmens.Update(sportsman);
+                    db.SaveChanges();
+                    response.IsSuccess = true;
+                }
+            }catch(Exception e)
+            {
+                response.IsSuccess = false;
+                response.ExceptionMessage = e.Message;
+            }
+            return response;
+        }
+
+        public Sportsman GetSportsmanById(int Id)
+        {
+            Sportsman sportsman = new Sportsman();
+            try
+            {
+                using (var db = new ApplicationContext())
+                {
+                    sportsman = db.Sportsmens.First(x => x.Id == Id);
+                    return sportsman;
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return sportsman;
         }
     }
 }
---
+++ BlazorApp/Util/ResponseObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Util
+{
+    public class ResponseObject
+    {
+        public bool IsSuccess { get; set; }
+        public string ExceptionMessage { get; set; }
+    }
+}
